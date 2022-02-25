0191200b0a040076316cb07f1426d0158a867a2c
nmwright57 <90349979+nmwright57@users.noreply.github.com>
2022-02-25 10:54:39 -0500

    Delete mapper1.py

--- mapper1.py
+++
@@ -1,9 +0,0 @@
-import re
-import sys
-
-pattern = re.compile(r'(?P<ip>\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}):(?P<hour>\d{2}):\d{2}.*? ')
-for line in sys.stdin:
-	match = pattern.search(line)
-	if match:
-		for hour in range(24):
-			print '%s\t%s' % ('[' + match.group('hour') + ':00' + ']' + match.group('ip'), 1)
