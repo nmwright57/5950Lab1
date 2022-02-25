0661d0fc785be3fa3961f77c64d6f740b17f6fdc
nmwright57 <90349979+nmwright57@users.noreply.github.com>
2022-02-25 10:54:54 -0500

    Delete reducer-2.py

--- reducer-2.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-from operator import itemgetter
-import sys
-
-for line in sys.stdin:
-  line = line.strip()
-   hour,count = line.split('\t',1)
-   print "%s\t%s" % (hour,count)
